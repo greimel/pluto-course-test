18568d5192738cc9e74e2eaef3164970b452269f
Fabian Greimel <fabgrei@gmail.com>
2020-11-09 22:31:00 +0100

    improve demand.jl

--- demand.jl
+++ demand.jl
@@ -41,7 +41,7 @@ We consider the following utility functions.
 # ╔═╡ f0fb1dc4-2100-11eb-1ff4-15c5b095ef74
 begin
 	u₁(x₁, x₂) = x₁ * √(x₂)
-	u₂(x₁, x₂) = x₁ + x₂ - 1/2 * x₂^2
+	u₂(x₁, x₂) = x₁ + x₂ - x₁^2 / 2
 	u₃(x₁, x₂) = x₁ + 6x₂
 	u₄(x₁, x₂) = min(x₁, 2x₂)
 	u₅(x₁, x₂) = min(x₁ + 3x₂, 4x₁ + x₂)
@@ -50,7 +50,7 @@ end
 # ╔═╡ 59a9646a-2107-11eb-2ce2-ab3390cae267
 md"""
 * $(@bind is_u1 CheckBox(default=true)) $$u(x_1, x_2) = x_1 \sqrt{x_2}$$
-* $(@bind is_u2 CheckBox()) $$u(x_1, x_2) = x_1 + x_2 - \frac{1}{2} x_2^2$$
+* $(@bind is_u2 CheckBox()) $$u(x_1, x_2) = x_1 + x_2 - \frac{1}{2} x_1^2$$
 * $(@bind is_u3 CheckBox()) $$u(x_1, x_2) = x_1 + 6 x_2$$
 * $(@bind is_u4 CheckBox()) $$u(x_1, x_2) = \min\{x_1, 2 x_2\}$$
 * $(@bind is_u5 CheckBox()) $$u(x_1, x_2) = \min\{x_1 + 3x_2, 4x_1 + x_2\}$$
@@ -84,7 +84,7 @@ We use a contour plot. First, we chose grids on the domain of $u$.
 
 # ╔═╡ a9777ed2-2102-11eb-1eb5-7193eed7a15b
 begin 
-	x₁_grid = LinRange(√eps(), 5, 200)
+	x₁_grid = LinRange(√eps(), 5, 205)
 	x₂_grid = LinRange(√eps(), 5, 200)
 	u_grid = u.(x₁_grid, x₂_grid')
 end;
@@ -100,12 +100,12 @@ end;
 
 # ╔═╡ 4529e838-2103-11eb-36d1-036bbe732597
 begin
-	plot(size = (300, 300), dpi = 40)
+	plot(size = (300, 300), dpi = 40, xlab = "x₁", ylab = "x₂")
 	# Plot some level curves that are automatically chosen
-	contour!(x₁_grid, x₂_grid, u_grid,
+	contour!(x₁_grid, x₂_grid, u_grid',
 		  	 color=:lightgray, colorbar=false)
 	# Then plot the level curves trough (1,1) and (2,2)
-	contour!(x₁_grid, x₂_grid, u_grid,
+	contour!(x₁_grid, x₂_grid, u_grid',
 		    levels = [k₁, k₂],
 		    color=:orange, colorbar=false, contourlabels=true)
 	scatter!([1, 2], [1, 2], label = false, color=:orange)
@@ -131,9 +131,9 @@ u(x₁, x₂)
 
 # ╔═╡ 1f350110-210d-11eb-12f5-0f48067dc473
 begin
-	MU₁ = diff(u(x₁, x₂), x₁)
-	MU₂ = diff(u(x₁, x₂), x₂)
-	MRS_sym = -MU₁ / MU₂
+	MU₁ = diff(u(x₁, x₂), x₁) |> simplify
+	MU₂ = diff(u(x₁, x₂), x₂) |> simplify
+	MRS_sym = -MU₁ / MU₂ |> simplify
 end
 
 # ╔═╡ cc6a5794-210d-11eb-01b4-c5611a880a6a
@@ -159,10 +159,10 @@ BC = p₁ * x₁ + p₂ * x₂ - m
 demand = solve([FOC, BC], [x₁, x₂])
 
 # ╔═╡ d022fa96-2113-11eb-25a0-15c97e2286a1
-demand1 = demand[x₁]
+demand1 = demand[x₁] |> simplify
 
 # ╔═╡ d8ed1ea4-2113-11eb-3d6d-db4d7786a40c
-demand2 = demand[x₂]
+demand2 = demand[x₂] |> simplify
 
 # ╔═╡ 1bece63a-210f-11eb-2895-4129fa8d5922
 md"""
@@ -206,17 +206,20 @@ md"""
 # ╔═╡ 9df19f84-2116-11eb-1ab0-096b36e6779c
 dx1dp2 = diff(demand1, p₂) |> simplify
 
-# ╔═╡ 9f69f668-2116-11eb-0d55-b5dc28790d35
-dx1dp2 > 0
-
-# ╔═╡ a3e9bd9a-2116-11eb-193a-5fe86161f211
-dx1dp2 < 0
-
-# ╔═╡ b2b9cf7c-2116-11eb-1e42-79253615cc9d
-dx1dp2 == 0
+# ╔═╡ c180ba7a-2198-11eb-0ee6-6d8937b1b136
+if dx1dp2 > 0
+	md"The derivative is positive. That is, the *demand for good one rises if price 2 rises*. Goods 1 and 2 are **substitutes**."
+elseif dx1dp2 < 0
+	md"The derivative is negative. That is, the *demand for good one falls if price 2 rises*. Goods 1 and 2 are **complements**."
+elseif dx1dp2 == 0
+	md"The derivative is zero. That is, the *demand for good one doesn't react to changes in price 2*. Goods 1 and 2 are **neither complements nor substitutes**."
+else
+	md"**SymPy failed to given an answer.**"
+end
 
 # ╔═╡ Cell order:
 # ╠═ae5ee7be-2102-11eb-2517-bf07efcd17c4
+# ╠═2fc7676e-2198-11eb-039f-8bf5ed3f4481
 # ╟─c6e808da-2100-11eb-22e8-1b96706f2fd6
 # ╟─b6c28632-2101-11eb-2a1b-19d744102a45
 # ╠═f0fb1dc4-2100-11eb-1ff4-15c5b095ef74
@@ -227,8 +230,8 @@ dx1dp2 == 0
 # ╟─52261572-210b-11eb-1a6e-f372102c29af
 # ╠═0ec2258c-2106-11eb-1724-21ec64a074dd
 # ╠═4529e838-2103-11eb-36d1-036bbe732597
-# ╠═c56ccf08-210b-11eb-05bf-736d5b0b848f
-# ╠═bc6411e0-210c-11eb-062a-2d6ebf7d9c96
+# ╟─c56ccf08-210b-11eb-05bf-736d5b0b848f
+# ╟─bc6411e0-210c-11eb-062a-2d6ebf7d9c96
 # ╠═a4a905ba-210c-11eb-282e-1b16883f280e
 # ╟─6cf6129c-210f-11eb-3244-65ab27c85105
 # ╟─fbd9a65a-210c-11eb-0a57-4335a3002ba5
@@ -242,12 +245,10 @@ dx1dp2 == 0
 # ╠═d022fa96-2113-11eb-25a0-15c97e2286a1
 # ╠═d8ed1ea4-2113-11eb-3d6d-db4d7786a40c
 # ╟─1bece63a-210f-11eb-2895-4129fa8d5922
-# ╠═8850eca0-2113-11eb-3f4e-c34994df0894
+# ╟─8850eca0-2113-11eb-3f4e-c34994df0894
 # ╟─52b4f674-2114-11eb-046f-0987e88c0098
-# ╠═ef4180dc-2113-11eb-2ea5-0148d7af3159
+# ╟─ef4180dc-2113-11eb-2ea5-0148d7af3159
 # ╟─1c185558-2115-11eb-01d7-13a209b82037
 # ╟─41bc0f42-2116-11eb-1792-695f97ab59ff
-# ╠═9df19f84-2116-11eb-1ab0-096b36e6779c
-# ╠═9f69f668-2116-11eb-0d55-b5dc28790d35
-# ╠═a3e9bd9a-2116-11eb-193a-5fe86161f211
-# ╠═b2b9cf7c-2116-11eb-1e42-79253615cc9d
+# ╟─9df19f84-2116-11eb-1ab0-096b36e6779c
+# ╟─c180ba7a-2198-11eb-0ee6-6d8937b1b136
