3a23c1bb522c2d97c82559d02b7f910a7ace5956
Fabian Greimel <fabgrei@gmail.com>
2023-08-19 00:51:09 +0200

    remove notebooks from template, adjust sidebar

--- content/sidebar data.jl
+++ content/sidebar data.jl
@@ -1,22 +1,22 @@
 Dict(
     :main => [
         "welcome" => collections["welcome"].pages,
-        "Preliminaries" => collections["preliminaries"].pages,
-        "Module 2: Social Science & Data Science" => collections["module2"].pages,
-        #"Module 3: Climate Science" => collections["module3"].pages,
+        #"Preliminaries" => collections["preliminaries"].pages,
+        "Module 1: Mathematical Preliminaries" => collections["mathematics"].pages,
+        "Module 2: Microeconomics" => collections["microeconomics"].pages,
 
     ],
     :about => Dict(
         :authors => [
-            (name = "Instructor One", url = "https://www.the-one.eu"),
+            (name = "Fabian Greimel", url = "https://www.greimel.eu"),
             (name = "Second Instructor", url = "https://www.the-second.com")
         ],
-        :title => "A Great Course",
-        :subtitle => "Even Greater Than You Thought",
-        :term => "Spring 2023",
-        :institution => "University of Greatness",
+        :title => "Microeconomics for AE",
+        :subtitle => "BSc Econometrics | BSc Business Analytics",
+        :term => "Fall 2023",
+        :institution => "University of Amsterdam",
         :institution_url => "http://www.uva.nl",
-        :institution_logo => "julia-logo-color.svg",
-        :institution_logo_darkmode => "julia-logo-dark.svg"
+        :institution_logo => "uva-logo.svg",
+       # :institution_logo_darkmode => "julia-logo-dark.svg"
     )
 )
